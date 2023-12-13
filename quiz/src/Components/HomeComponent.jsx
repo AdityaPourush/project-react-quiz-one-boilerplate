ac08b63e61b9c4d8835a1d298f67de7e6a562dc7
AdityaPourush <adityapourush11@outlook.com>
2023-12-13 23:53:46 +0530

    done

---
+++ quiz/src/Components/HomeComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Home extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return(
+            <div className='page1'>
+                <div id='page1'>
+                    <h1>Quiz App</h1>
+                    <button>Play</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
